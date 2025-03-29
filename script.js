b065eb385317c82864506245c896dd1e71564806
utkarsha255 <utkarshabhapkar3@gmail.com>
2025-03-29 22:59:06 +0530

    create first page

--- script.js
+++ script.js
@@ -1,24 +1,65 @@
+// Array of channel images
 var favChannels = [
-   { img_url: "/AMAZON---CLONE/img/lionsgate.jpg"},
-   { img_url: "/AMAZON---CLONE/img/discovery.jpg"},
-   { img_url: "/AMAZON---CLONE/img/bbcplayer.jpg"},
-   { img_url: "/AMAZON---CLONE/img/manorama.jpg"},
-   { img_url: "/AMAZON---CLONE/img/hoichoi.jpg"},
-   { img_url: "/AMAZON---CLONE/img/chaupal.jpg"},
-   { img_url: "/AMAZON---CLONE/img/times.jpg"},
-   { img_url: "/AMAZON---CLONE/img/sony.jpg"},
-   { img_url: "/AMAZON---CLONE/img/mgm.jpg"},
-];
-
-favChannels.map(function (elem){
-    var div = document.createElement("div");
-    div.setAttribute("class","channelDiv");
-
-    var image = document.createElement("img");
-    image.setAttribute("src",elem.img_url);
-    div.append(image);
-    document.querySelector(".channel-btn").append(div);
-
-});
-
-localStorage.setItem("favChannels", JSON.stringify(favChannels));
+    { img_url: "img/lionsgate.jpg" },
+    { img_url: "img/discovery.jpg" },
+    { img_url: "img/bbcplayer.jpg" },
+    { img_url: "img/manorama.jpg" },
+    { img_url: "img/hoichoi.jpg" },
+    { img_url: "img/chaupal.jpg" },
+    { img_url: "img/times.jpg" },
+    { img_url: "img/sony.jpg" },
+    { img_url: "img/mgm.jpg" },
+  ]
+  
+  // Create and append channel divs
+  function renderChannels() {
+    var channelContainer = document.querySelector(".channel-btn")
+    channelContainer.innerHTML = "" // Clear existing content
+  
+    favChannels.forEach((elem) => {
+      var div = document.createElement("div")
+      div.setAttribute("class", "channelDiv")
+  
+      var image = document.createElement("img")
+      image.setAttribute("src", elem.img_url)
+      image.setAttribute("alt", "Channel logo")
+  
+      div.appendChild(image)
+      channelContainer.appendChild(div)
+    })
+  }
+  
+  // Save channels to localStorage
+  function saveChannels() {
+    localStorage.setItem("favChannels", JSON.stringify(favChannels))
+  }
+  
+  // Check for responsive behavior
+  function checkResponsive() {
+    const width = window.innerWidth
+    const textOverImages = document.querySelectorAll(".text-over-image")
+  
+    if (width <= 576) {
+      textOverImages.forEach((div) => {
+        div.style.justifyContent = "flex-start"
+        div.style.paddingTop = "100px"
+      })
+    } else {
+      textOverImages.forEach((div) => {
+        div.style.justifyContent = "center"
+        div.style.paddingTop = "40px"
+      })
+    }
+  }
+  
+  // Initialize
+  document.addEventListener("DOMContentLoaded", () => {
+    renderChannels()
+    saveChannels()
+    checkResponsive()
+  
+    // Add resize listener for responsive behavior
+    window.addEventListener("resize", checkResponsive)
+  })
+  
+  
